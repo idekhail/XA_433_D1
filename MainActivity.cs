dc5d670818d04ce7d2ac097130bcc4cd9e445a7d
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-04-03 11:25:30 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+
+using AndroidX.AppCompat.App;
+
+namespace XA_443_D1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var name = FindViewById<EditText>(Resource.Id.name);
+            var ok = FindViewById<Button>(Resource.Id.ok);
+            var clear = FindViewById<Button>(Resource.Id.clear);
+            var show = FindViewById<TextView>(Resource.Id.show);
+            Button go = FindViewById<Button>(Resource.Id.go);
+
+            ok.Click += delegate
+            {
+                show.Text = name.Text;
+            };
+
+            clear.Click += delegate
+            {
+                name.Text = "";
+                show.Text = "Show";
+            };
+
+            go.Click += delegate {
+                Intent i = new Intent(this, typeof(SecondActivity));
+                i.PutExtra("name", name.Text);
+                StartActivity(i);
+
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ SecondActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XA_443_D1
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Create your application here
+
+            SetContentView(Resource.Layout.activity_second);
+
+            TextView show = FindViewById<TextView>(Resource.Id.show);
+            Button back = FindViewById<Button>(Resource.Id.back);
+            Button close = FindViewById<Button>(Resource.Id.close);
+
+            show.Text = Intent.GetStringExtra("name");
+
+            back.Click += delegate { 
+                    Intent i = new Intent(this, typeof(MainActivity));
+                    StartActivity(i);
+            };
+
+            close.Click += delegate { System.Environment.Exit(0); };
+
+        }
+    }
+}
